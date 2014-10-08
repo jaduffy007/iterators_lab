ab7d91529af2be9ba5b20369df1096622a0dbb6e
JamesDuffy <jaduffy007@gmail.com>
2014-10-08 08:45:01 -0700

    week2 day2

--- iterators.js
+++ iterators.js
@@ -7,7 +7,38 @@ var iterators = {
         }
       }
       return max;
+    },
+
+    min: function (numList){
+      var min =  Infinity;
+      for (var i = 0; i < numList.length; i++) {
+        if (numList[i] < min) {
+          min = numList[i];
+        }
+      }
+      return min;
+    },
+
+    map: function (myArray, callback){
+      var newArray = [];
+      for (var i = 0; i < myArray.length; i++) {
+        newArray.push(callback(myArray[i]));
+      }
+      return newArray;
+    },
+
+    each: function (myArray,callback){
+      // iterate over array, [i]*2
+      for (var i = 0; i < myArray.length; i++) {
+        callback(myArray[i]);
     }
-  };
+  }
+},
+    filter: function (myArray, callback) {
+      // iterate over array, [i]%2
+    };
+
+
+
 
 module.exports = iterators;
--- spec/iterators_test.js
+++ spec/iterators_test.js
@@ -1,9 +1,10 @@
+
+
 var expect = require('chai').expect;
 var Iterators = require('../iterators');
 
 describe('Iterators', function () {
   describe('#max', function () {
-
     // cleaner to declare the variable outside of the beforeEach function
     var myArr;
 
@@ -21,4 +22,84 @@ describe('Iterators', function () {
       expect(Iterators.max(myArr)).to.equal(-Infinity);
     });
   });
+ 
+  // min
+  describe("min", function() {
+    // cleaner to declare the variable outside of the beforeEach function
+    var myArr;
+
+    beforeEach(function () {
+      myArr = [14, 22, 67, 34];
+    });
+  it("should return the lowest value number", function(){
+    // Call what you want to test
+    var result = Iterators.min(myArr);
+    // Compare result with what you expect.
+    expect(result).to.equal(14);
+    });
+  });
+
+  // map test
+  describe("map", function(){
+    var myArr;
+    beforeEach(function () {
+      myArr = [2, 4, 8, 10];
+    });
+
+    var timesTwo = function(x){
+        return x*2;
+    };
+
+    it("should create a new array of values*2", function(){
+      // Test starts here, call map function
+      var result = Iterators.map(myArr, timesTwo);
+
+      // now compare result with expected
+      expect(result).to.deep.equal([4, 8, 16, 20]);
+    });
+  });
+
+  describe("each", function(){
+    var myArr;
+    beforeEach(function () {
+      myArr = [2, 4, 8, 10];
+    });
+
+    it("should create a new array of values*2", function(){
+      // Test starts here, call map function
+      var resultArray = [];
+
+      Iterators.each(myArr, function(x){
+        resultArray.push(x*2);
+      });
+
+      // now compare result with expected
+      expect(resultArray).to.deep.equal([4, 8, 16, 20]);
+    });
+  });
+
 });
+
+// Filter test
+
+describe("filter", function() {
+  var myArr;
+    beforeEach(function () {
+      myArr = [2, 4, 8, 10, 13, 17];
+    });
+    resultArray = [];
+
+    Iterators.filter(myArr, function(x){
+      resultArray.push(x % 2 === 0);
+
+  it("should create a new array with only even numbers", function(){
+    expect(resultArray).to.equal([2, 4, 8, 10]);
+    
+  });
+});
+});
+
+
+
+
+
